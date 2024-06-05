cda1a866128a23d313ff7a2760b72bd1b8706e24
Paramjit Singh <sparamjit92@gmail.com>
2024-06-05 15:57:11 +0530

    Get secrets from the Azure

--- AzureKeyVault.Maui/MainViewModel.cs
+++ AzureKeyVault.Maui/MainViewModel.cs
@@ -1,8 +1,4 @@
-﻿
-
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
@@ -18,63 +14,70 @@ namespace AzureKeyVault.Maui
         string label2Text;
 
         [ObservableProperty]
-        string label3Text;
+        string functionUrl;
 
-        string? keyVaultUri ;
-        string? clientSecret;
-        string? clientId ;
-        string? tenantId ;
+        [ObservableProperty]
+        string secretNames;
 
-        string? environment ;
+
+        static HttpClient httpClient = new HttpClient();
 
         public MainViewModel()
         {
+            // Load the FunctionURL and SecretNames from the appsettings.json file. The appsettings.json file is embedded in the project and is ignored by Git. 
             var a = Assembly.GetExecutingAssembly();
             using var stream = a.GetManifestResourceStream("AzureKeyVault.Maui.appsettings.json");
             IConfiguration? config = new ConfigurationBuilder().AddJsonStream(stream).Build();
-           keyVaultUri = config["keyVault:url"];
-           clientSecret = config["keyVault:clientSecret"];
-           clientId = config["keyVault:clientId"];
-           tenantId = config["keyVault:tenantId"];
+            FunctionUrl = config["function:url"];
+            SecretNames = "Secret1Name,Secret2Name";
 
-            string? environment = config["Environment"];
         }
 
         public async Task LoadKeyVaultSecret()
         {
-            // Replace with your key vault secret name
-            string key1 = "firstSecretKey";
-            string key2 = "secondSecretKey";
-            string key3 = "thirdSecretKey";
+            
+            string secrets =await GetSecretFromAzure();
 
-            string secret1 =await GetSecretFromAzure(key1);
-            string secret2 = await GetSecretFromAzure(key2);
-            string secret3 = await GetSecretFromAzure(key3);
+           // Split the secrets into an array
+            string[] secret1 = secrets.Split(",");
 
-
-            Label1Text = $"Secret1:  {secret1}";
-            Label2Text = $"Secret2:  {secret2}";
-            Label3Text = $"Secret3:  {secret3}";
+            // the loop is running twice because we are getting two secrets from the function. Use the secrets in the project as needed.    
+            for(int i=0; i<2;i++)
+            {
+                string secret = secret1[i];
+                string secretKey = secret.Split(":")[0].Replace("{", string.Empty).Replace("\"", string.Empty);
+                string secretValue = secret.Split(":")[1].Replace("}", string.Empty).Replace("\"", string.Empty);
+                if (i==0)
+                {
+                    Label1Text = $"{secretKey}:  {secretValue}";
+                }
+                else if (i==1)
+                {
+                    Label2Text = $"{secretKey}:  {secretValue}";
+                }
+            }   
+           
+           
         }
 
-        private async Task<string> GetSecretFromAzure(string key)
+        private async Task<string> GetSecretFromAzure()
         {
             try
             {
-               
-                ClientSecretCredential clientSecretCredential = new ClientSecretCredential(tenantId, clientId,clientSecret);
-                var client = new SecretClient(new Uri(keyVaultUri), clientSecretCredential);
-                KeyVaultSecret secret = await client.GetSecretAsync(key);
-                return secret.Value;    
-               
+                Uri uri = new Uri($"{FunctionUrl}&SecretNames={SecretNames}");
+
+                var getRequest = await httpClient.GetAsync(uri);
+                var secretJson = await getRequest.Content.ReadAsStringAsync();
+                return secretJson;
+
+
             }
             catch (Exception ex)
             {
 
                 
             }
-
-            return "dummy secret";
+            return string.Empty;
         }   
     }
 }
